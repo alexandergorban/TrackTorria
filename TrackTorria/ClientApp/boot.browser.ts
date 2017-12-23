5df0943d7ecedbe354daac12f74b2b5873c376c3
Alexander Gorban <gorban.alexander.y@gmail.com>
2017-12-23 20:08:15 +0200

    Remove bootstrap

--- TrackTorria/ClientApp/boot.browser.ts
+++ TrackTorria/ClientApp/boot.browser.ts
@@ -1,7 +1,6 @@
 import 'reflect-metadata';
 import 'zone.js';
 import 'hammerjs';
-import 'bootstrap';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.browser.module';
