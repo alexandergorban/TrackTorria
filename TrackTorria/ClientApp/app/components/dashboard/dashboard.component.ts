54c23522fc89cd912ce326a81607796921083fed
Alexander Gorban <gorban.alexander.y@gmail.com>
2017-12-25 02:26:20 +0200

    Added npm less

--- TrackTorria/ClientApp/app/components/dashboard/dashboard.component.ts
+++ TrackTorria/ClientApp/app/components/dashboard/dashboard.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.less']
 })
 export class DashboardComponent {
 
