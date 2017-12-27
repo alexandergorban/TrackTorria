745c4e219ea0e93b368fb96a3574c13bdbaf048f
Alexander Gorban <gorban.alexander.y@gmail.com>
2017-12-27 16:41:55 +0200

    Fixed style url links

--- TrackTorria/ClientApp/app/components/app/app.component.ts
+++ TrackTorria/ClientApp/app/components/app/app.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.less']
 })
 export class AppComponent {
 
