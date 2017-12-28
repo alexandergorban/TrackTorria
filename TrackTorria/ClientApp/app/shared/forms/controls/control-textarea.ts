9f6d235bdc812fb2b203a3e7d0ae3ade7dd1f028
Alexander Gorban <gorban.alexander.y@gmail.com>
2017-12-28 11:31:40 +0200

    Added basic form control

---
+++ TrackTorria/ClientApp/app/shared/forms/controls/control-textarea.ts
@@ -0,0 +1,8 @@
+﻿import { ControlBase } from './control-base';
+
+export class ControlTextarea extends ControlBase<string> {
+  constructor(options: any = {}) {
+    super(options);
+    this.type = options.type || 'textarea';
+  }
+}
