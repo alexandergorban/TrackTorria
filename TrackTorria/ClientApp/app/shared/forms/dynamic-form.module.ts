3035bd5745ba55244aa75667b71cb5870751b751
Alexander Gorban <gorban.alexander.y@gmail.com>
2017-12-28 14:57:24 +0200

    Added dynamic form module

---
+++ TrackTorria/ClientApp/app/shared/forms/dynamic-form.module.ts
@@ -0,0 +1,35 @@
+﻿import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Components
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { DynamicFormControlComponent } from './dynamic-form-control/dynamic-form-control.component';
+import { ErrorSummaryComponent } from './error-summary/error-summary.component';
+
+// Services
+import { FormControlService } from './form-control.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    DynamicFormComponent,
+    DynamicFormControlComponent,
+    ErrorSummaryComponent
+  ],
+  exports: [
+    DynamicFormComponent,
+    DynamicFormControlComponent,
+    ErrorSummaryComponent
+  ],
+  providers: [
+    FormControlService
+  ]
+})
+export class DynamicFormModule {
+
+}
