c3011eac82683fd2ce400e9ac88fe6b02ec4c775
Alexander Gorban <gorban.alexander.y@gmail.com>
2017-12-29 22:18:37 +0200

    Added card model

---
+++ TrackTorria/Models/CardsDataStore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackTorria.Models
+{
+    public class CardsDataStore
+    {
+        public static CardsDataStore Current { get; } = new CardsDataStore();
+        public List<CardDto> Cards { get; set; }
+
+        public CardsDataStore()
+        {
+            // todo remove dummy data
+            Cards = new List<CardDto>()
+            {
+                new CardDto()
+                {
+                    Id = 1,
+                    Name = "First card",
+                    Description = "Description first card",
+                    CreatedAt = new DateTime(2017, 07, 20),
+                    Stage = Stage.ToDo
+                },
+                new CardDto()
+                {
+                    Id = 2,
+                    Name = "Second card",
+                    Description = "Description second card",
+                    CreatedAt = new DateTime(2017, 07, 20),
+                    Stage = Stage.InProgress
+                },
+                new CardDto()
+                {
+                    Id = 3,
+                    Name = "Third card",
+                    Description = "Description third card",
+                    CreatedAt = new DateTime(2017, 07, 20),
+                    Stage = Stage.InProgress
+                }
+            };
+
+        }
+    }
+}
---
+++ TrackTorria/Models/Stage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackTorria.Models
+{
+    public enum Stage
+    {
+        BackLog = 1,
+        ToDo = 2,
+        InProgress = 3,
+        CodeReview = 4,
+        Merged = 5,
+        InTest = 6,
+        Tested = 7,
+        Deployed = 8,
+        Done = 9
+    }
+}
