df9ddfec7aea25a45831a17c71bac279c686a5d1
Alexander Gorban <gorban.alexander.y@gmail.com>
2017-12-29 22:18:55 +0200

    Added CardsController

---
+++ TrackTorria/Controllers/CardsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TrackTorria.Models;
+
+namespace TrackTorria.Controllers
+{
+    [Route("api/cards")]
+    public class CardsController : Controller
+    {
+        [HttpGet()]
+        public JsonResult GetCards()
+        {
+            return new JsonResult(CardsDataStore.Current.Cards);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCard(int id)
+        {
+            return new JsonResult(CardsDataStore.Current.Cards.FirstOrDefault(c => c.Id == id));
+        }
+
+    }
+}
