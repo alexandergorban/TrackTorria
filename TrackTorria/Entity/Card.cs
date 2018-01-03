4088ae5be3c4e0f26f080449c34712700790cf45
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-01-03 21:37:11 +0200

    Created Entity Classes (Card, Comment)

---
+++ TrackTorria/Entity/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using TrackTorria.Models;
+
+namespace TrackTorria.Entity
+{
+    public class Card
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public Stage Stage { get; set; }
+
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
---
+++ TrackTorria/Entity/Comment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using TrackTorria.Models;
+
+namespace TrackTorria.Entity
+{
+    public class Comment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string User { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        [Required]
+        public Stage Stage { get; set; }
+
+        [Required]
+        public DateTime AddedAt { get; set; }
+    }
+}
